bce3653cdca79aacb905e9cfa3be0c557fc2ce29
hajrullinbulat <bulat.hajrullin@dz.ru>
2017-05-05 14:53:36 +0300

    advert query fix

--- db-connector/src/main/java/com/findandplay/Queries/AdvertQuery.java
+++ db-connector/src/main/java/com/findandplay/Queries/AdvertQuery.java
@@ -7,7 +7,7 @@ public interface AdvertQuery {
     //todo order by last action
     String searchAdvertsWithPaging =
             "SELECT\n" +
-                    "  adv.id                        AS advert_id,\n" +
+                    "  adv.advert_id,\n" +
                     "  adv.advert_sport,\n" +
                     "  adv.advert_info,\n" +
                     "  adv.advert_min_level,\n" +
@@ -21,13 +21,22 @@ public interface AdvertQuery {
                     "  checked_user.id               AS checked_user_id,\n" +
                     "  checked_user.user_name        AS checked_user_name,\n" +
                     "  checked_user.msisdn           AS checked_user_msisdn\n" +
-                    "FROM adverts adv\n" +
+                    "FROM (SELECT\n" +
+                    "        innder_adv.id AS advert_id,\n" +
+                    "        innder_adv.advert_sport,\n" +
+                    "        innder_adv.advert_info,\n" +
+                    "        innder_adv.advert_min_level,\n" +
+                    "        innder_adv.advert_persons_count,\n" +
+                    "        innder_adv.advert_status,\n" +
+                    "        innder_adv.advert_created,\n" +
+                    "        innder_adv.advert_author_id\n" +
+                    "      FROM adverts innder_adv\n" +
+                    "      ORDER BY innder_adv.id\n" +
+                    "      LIMIT ?\n" +
+                    "      OFFSET ?) adv\n" +
                     "  INNER JOIN users author ON adv.advert_author_id = author.id\n" +
-                    "  LEFT JOIN checked_adverts ch_adv ON adv.id = ch_adv.advert_id\n" +
+                    "  LEFT JOIN checked_adverts ch_adv ON adv.advert_id = ch_adv.advert_id\n" +
                     "  LEFT JOIN users checked_user ON ch_adv.user_id = checked_user.id\n" +
                     "WHERE adv.advert_sport = ?\n" +
-                    "--   todo last_action\n" +
-                    "ORDER BY adv.id\n" +
-                    "LIMIT ?\n" +
-                    "OFFSET ?";
+                    "--   todo last_action";
 }
